adfb00b80877e96ef6410e85de7e5de79322608c
Artyom Danilov <48842726+defytheflow@users.noreply.github.com>
2019-08-13 19:56:55 +0300

    Update crack.c

--- week2/crack.c
+++ week2/crack.c
@@ -3,7 +3,9 @@
 #include <crypt.h>
 #include <string.h>
 
-const string ALPHA[52] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",                               "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",                               "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+const string ALPHA[52] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+                          "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                          "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
 string concat(const string s1, const string s2);
 bool crack1(string salt_hash, string salt, string hash);
